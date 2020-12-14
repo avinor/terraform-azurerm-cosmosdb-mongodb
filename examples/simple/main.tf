eb2f642e362c8de2de44b42cc6d20c029948fbd6
Yngve Hansen <yngve.hansen2@avinor.no>
2020-12-14 15:38:51 +0100

    Run terraform fmt for examples

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -9,7 +9,7 @@ module "cosmosdb" {
 
   databases = {
     mydb1 = {
-      throughput  = 400
+      throughput = 400
       collections = [
         { name = "col0", shard_key = "somekey_0", throughput = 1000 },
         { name = "col1", shard_key = "somekey_1", throughput = null }
