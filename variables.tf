5d8ce5a621ed65b43670416841d1d3f10851d2a2
Pavlo <pavlo.pryh@gmail.com>
2021-03-12 15:41:07 +0100

    Update description
    Updated description to make it less confusing.

--- variables.tf
+++ variables.tf
@@ -41,7 +41,7 @@ variable "diagnostics" {
 }
 
 variable "ip_range_filter" {
-  description = "CosmosDB Firewall Support: This value specifies the set of IP addresses or IP address ranges in CIDR form to be included as the allowed list of client IP's for a given database account. IP addresses/ranges must be a comma separated list of strings"
+  description = "CosmosDB Firewall Support: This value specifies the set of IP addresses or IP address ranges in CIDR form to be included as the allowed list of client IP's for a given database account."
   type        = list(string)
   default     = []
 }
